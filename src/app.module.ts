b13477b89ab7f6c0552a25a4369220a08c2d9311
wicomon <wcv.94@hotmail.com>
2025-11-11 20:20:39 -0500

    gql setup

---
+++ src/app.module.ts
@@ -0,0 +1,44 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import {
+  ApolloServerPluginLandingPageLocalDefault,
+  ApolloServerPluginLandingPageProductionDefault,
+} from '@apollo/server/plugin/landingPage/default';
+import { ConfigModule } from '@nestjs/config';
+import { join } from 'path';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      // load: [EnvVaribales],
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      // rootValue: '/',
+      path: '/gql',
+      plugins: [
+        process.env.NODE_ENV === 'development'
+          ? ApolloServerPluginLandingPageLocalDefault()
+          : ApolloServerPluginLandingPageProductionDefault(),
+      ],
+      // formatError: (err) => ({
+      //   message: err.message,
+      //   // status: err.extensions.code,
+      //   // extensions: err.extensions,
+      //   error: err.extensions.originalError['error'],
+      //   code: err.extensions.originalError['statusCode'],
+      //   originalMessage: err.extensions.originalError['message'],
+      // }),
+    }),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    // logger: ['error', 'warn', 'debug', 'fatal','verbose']
+  });
+  const acceptedUrls = process.env.ACCEPTED_URLS?.split(', ');
+
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  // console.log({acceptedUrls})
+  app.enableCors({
+    origin: acceptedUrls,
+  });
+
+  // console.log(process.env.PORT);
+  const PORT = process.env.PORT || '4030';
+  await app.listen(PORT);
+  console.log(`App running on port ${PORT}`);
+}
+bootstrap();
