8e533cb131f7b6ced408395dff6c26411d40c159
wicomon <wcv.94@hotmail.com>
2025-11-23 23:32:15 -0500

    feat(common): implement CommonService for error handling

---
+++ src/common/services/common.service.ts
@@ -0,0 +1,43 @@
+import {
+  BadRequestException,
+  HttpException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { Prisma } from 'generated/prisma/client';
+
+@Injectable()
+export class CommonService {
+  handleErrors(error: any) {
+    console.log(error);
+    if (error instanceof HttpException) {
+      throw error;
+    }
+
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      // Handle known Prisma errors
+      console.log('is instance');
+      switch (error.code) {
+        case 'P2000':
+          throw new BadRequestException('Provided value is too long');
+        case 'P2002':
+          throw new BadRequestException('Unique constraint failed');
+        case 'P2003':
+          throw new BadRequestException('Foreign key constraint failed');
+        case 'P2005':
+          throw new BadRequestException('Invalid Field Type');
+        case 'P2025':
+          throw new NotFoundException('Record not found');
+        default:
+          throw new InternalServerErrorException('Database error');
+      }
+    } else if (error.status) {
+      // Handle other known errors
+      throw new NotFoundException(error.response);
+    } else {
+      // Handle unknown errors
+      throw new InternalServerErrorException('Internal Error');
+    }
+  }
+}
